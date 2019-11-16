e7083f515b7b1c6d8ad45c721146b4d2f34210d4
Jamie Perkins <jamie.perkins@gmail.com>
2019-11-16 07:04:58 -0700

    install webauthn pkg

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import * as bodyParser from 'body-parser';
 import * as cookieSession from 'cookie-session';
 import * as cookieParser from 'cookie-parser';
 import * as crypto from 'crypto';
+import * as WebAuthn from 'webauthn';
 
 // app
 const port = process.env.PORT || 3000;
@@ -22,7 +23,24 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../dist/ngx-webauthn')));
 
 // webauthn routes
-// app.use('/webauthn', webuathnauth)
+const webauthn = new WebAuthn({
+  origin: `http://localhost:${port}`,
+  usernameField: 'email', // field that uniquely id's user
+  userFields: {
+    email: 'email',
+    name: 'displayName',
+  },
+  // using default in-memory "db"
+  // OR
+  // store: {
+  //   put: async (id, value) => void,
+  //   get: async (id) => User,
+  //   search: async (search) =>  { [username]: User },
+  //   delete: async (id) => boolean,
+  // },
+  rpName: 'Inorganik Produce, inc.',
+});
+app.use('/webauthn', webauthn.initialize());
 
 // init
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
